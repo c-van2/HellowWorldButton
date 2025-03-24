a3beaa7750f832288371d197ba99b9042a9f081e
c.van1ssa <winglamc1@student.unimelb.edu.au>
2025-03-24 23:49:50 +0800

    Build: ButtonReturnApi

--- HellowWorldButton/Program.cs
+++ HellowWorldButton/Program.cs
@@ -6,22 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Register HttpClient
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["BaseUrl"] ?? "https://localhost:5015") });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
+// Map API endpoint
+app.MapGet("/api/hello", () => "Hello World");
+
+// Map Razor Components
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
